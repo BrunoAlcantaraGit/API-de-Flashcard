cf218632e31fa425b9a162b3c5fd6fa231daebb5
Bruno Alcantara <120646838+BrunoAlcantaraGit@users.noreply.github.com>
2024-01-26 08:58:19 -0300

    frist commit

---
+++ src/main/java/br/com/dio/reactiveflashecard/ReactiveFlashecardApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.reactiveflashecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFlashecardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFlashecardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/reactiveflashecard/ReactiveFlashecardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.reactiveflashecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashecardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
